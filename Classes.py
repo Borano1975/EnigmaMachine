f1c4af2af45396cae3b3b0426ba157f726b56467
Borano Llana <boranollana@gmail.com>
2021-03-22 18:17:25 +0100

    Initial Iniiiitt

--- Classes.py
+++ Classes.py
@@ -1,39 +1,19 @@
-class Rotor:
-    def __init__(self, rotor, position):
-        self.rotor = [char for char in rotor]
-        self.position = position % 26
-        self.ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+class Enigma():
+    def __init__(self, reflector, rotor1, rotor2, rotor3, position1, position2, position3, plugboard):
+        if reflector is None:
+            pass
+        else:
+            self.reflector = reflector
+            self.rotor1 = rotor1
+            self.rotor2 = rotor2
+            self.rotor3 = rotor3
+            self.position1 = position1 % 26
+            self.position2 = position2 % 26
+            self.position3 = position3 % 26
+            self.plugboard = plugboard
+            self.ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    def runThroughFront(self, letter):
-        for x in range(26):
-            if letter == self.ALPHABET[x]:
-                letter = self.rotor[(x - self.position) % 26]
-                return letter
-
-    def runThroughBack(self, letter):
-        for x in range(26):
-            if letter == self.rotor[x]:
-                letter = self.ALPHABET[(x-self.position) % 26]
-                return letter
-
-
-class Reflector:
-    def __init__(self, reflector):
-        self.reflector = [char for char in reflector]
-        self.ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-    def runThrough(self, letter):
-        for x in range(0, len(self.reflector)):
-            if letter == self.reflector[x]:
-                letter = self.ALPHABET[x]
-                return letter
-
-
-class Plugboard:
-    def __init__(self, plugboard):
-        self.plugboard = plugboard
-
-    def runThrough(self, letter):
+    def runThroughPlugboard(self, letter):
         for x in range(10):
             if self.plugboard[x][0] == letter:
                 return self.plugboard[x][1]
@@ -41,15 +21,48 @@ class Plugboard:
                 return self.plugboard[x][0]
         return letter
 
+    def runThroughReflector(self, letter):
+        for x in range(0, len(self.reflector)):
+            if letter == self.reflector[x]:
+                letter = self.ALPHABET[x]
+                return letter
 
-class Enigma:
-    def __init__(self, reflector, rotor1, rotor2, rotor3, position1, position2, position3, plugboard):
-        if reflector or rotor1 or position1 is None:
-            pass
-        else:
-            self.reflector = Reflector(reflector)
-            self.rotor1 = Rotor(rotor1, position1)
-            self.rotor2 = Rotor(rotor2, position2)
-            self.rotor3 = Rotor(rotor3, position3)
-            self.plugboard = Plugboard(plugboard)
-            print("great sakses.")
+    def runThroughRotorFront(self, letter, num):
+        for x in range(26):
+            if letter == self.ALPHABET[x]:
+                if num == 1:
+                    letter = self.rotor1[(x - self.position1) % 26]
+                    return letter
+                if num == 2:
+                    letter = self.rotor2[(x - self.position2) % 26]
+                    return letter
+                if num == 3:
+                    letter = self.rotor3[(x - self.position3) % 26]
+                    return letter
+
+    def runThroughRotorBack(self, letter, num):
+        if num == 1:
+            for x in range(26):
+                if letter == self.rotor1[x]:
+                    letter = self.ALPHABET[(x - self.position1) % 26]
+                    return letter
+        if num == 2:
+            for x in range(26):
+                if letter == self.rotor2[x]:
+                    letter = self.ALPHABET[(x - self.position2) % 26]
+                    return letter
+        if num == 3:
+            for x in range(26):
+                if letter == self.rotor3[x]:
+                    letter = self.ALPHABET[(x - self.position3) % 26]
+                    return letter
+
+    def fixPositioning(self):
+        self.position1 += 1
+        if self.position1 >= 26:
+            self.position1 = self.position1 % 26
+            self.position2 += 1
+            if self.position2 >= 26:
+                self.position2 = self.position2 % 26
+                self.position3 += 1
+                self.position3 = self.position3 % 26
